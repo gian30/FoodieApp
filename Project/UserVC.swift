158b689bae486017cf0fa918b032954e5a1e7e57
lluc_lovec <gianlucalvs@gmail.com>
2018-02-06 23:15:17 +0100

    log in implemented and finished

---
+++ Project/UserVC.swift
@@ -0,0 +1,39 @@
+//
+//  UserVC.swift
+//  Project
+//
+//  Created by Gianluca Lo Vecchio on 6/2/18.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import UIKit
+
+class UserVC: UIViewController {
+
+    
+    var uid: String!
+    var emailField: String!
+    var passwordField: String! 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -8,15 +8,56 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
+class ViewController: UIViewController, UIAlertViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBAction func registerButton(_ sender: Any) {
+        self.goToCreateUserVC()
+    }
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
+    var uid: String!
     @IBAction func signInTapped(_ sender: Any) {
-      
-    
+        if let email = self.emailField.text, let password = self.passwordField.text {
+                Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                    if error == nil {
+                    if let user = user {
+                        self.uid = user.uid
+                        self.goToFeedVC()
+                        }
+                    }else{
+                        let alertView = UIAlertView(title: "Contraseña incorrecta", message: "La contraseña que has escrito no es correcta. Vuelve a intentarlo.", delegate: self as! UIAlertViewDelegate, cancelButtonTitle: "Volver a intentarlo")
+                        
+                        // Configure Alert View
+                        alertView.tag = 1
+                        
+                        // Show Alert View
+                        alertView.show()
+                    }
+                }
+        
+       
+        }}
+    func goToCreateUserVC(){
+        performSegue(withIdentifier: "signUp", sender: nil)
+    }
+    func goToFeedVC(){
+        performSegue(withIdentifier: "toFeed", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "signUp"{
+            if let destination = segue.identifier as? UserVC {
+                if uid != nil {
+                    destination.uid = uid
+                }
+                if emailField.text != nil {
+                    destination.emailField = emailField.text
+                }
+                if passwordField.text != nil{
+                    destination.passwordField = passwordField.text
+                }
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
