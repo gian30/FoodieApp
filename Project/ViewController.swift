c94fdad1f2a365618250861a0f1f6f1c66b6c6b3
lluc_lovec <gianlucalvs@gmail.com>
2018-02-06 15:23:37 +0100

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Gianluca Lo Vecchio on 6/2/18.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
