dd551d52b4619873617b53eca3f1e943cd494941
gian30 <gianlucalvs@gmail.com>
2018-05-25 20:35:27 +0200

    Feed updated, description and photo added support

---
+++ Project/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Project
+//
+//  Created by Gianluca Lo Vecchio on 25/5/18.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
