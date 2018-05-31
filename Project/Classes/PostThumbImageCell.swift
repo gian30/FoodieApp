4544db4d19aa0fb2c9153a75019b1a84b5d9fa8a
Project <project@iMac-3.local>
2018-05-31 20:03:26 +0200

    nova estructura

---
+++ Project/Classes/PostThumbImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  PostThumbImageCell.swift
+//  Project
+//
+//  Created by DAM on 29/5/18.
+//  Copyright © 2018 DAM. All rights reserved.
+//
+
+import UIKit
+
+class PostThumbImageCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+}
